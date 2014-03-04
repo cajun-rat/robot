8e96c8fb071eb077d99fa905487e220cf4e8068a
Ubuntu <ubuntu@ip-10-234-221-186.eu-west-1.compute.internal>
2014-03-04 14:00:53 +0000

    Count votes

--- server/app.js
+++ server/app.js
@@ -9,6 +9,72 @@ var fs = require('fs');
 
 var logfile = fs.createWriteStream('votes.log', {flags:'a', mode:'644', encoding:null});
 
+function VoteCounter() {
+	this.votes = {};
+	this.winner = 'centre-stop';
+	this.runningtimer = false;
+}
+
+VoteCounter.prototype.setVote = function(user,vote) {
+	this.votes[user] = {vote:vote, time:Date.now()};
+	this.recalculate();
+};
+
+VoteCounter.prototype.recalculate = function () {
+	var expirytime = Date.now() - (1000*10); // 10s vote timeout
+	var goodvotes = {};
+	var outcome = {};
+	var oldestvote = 0;
+
+	for (var k in this.votes) {
+		if (!this.votes.hasOwnProperty(k)) {
+			continue;
+		}
+		var vote = this.votes[k];
+		if (vote.time < expirytime) {
+			continue;
+		}
+		if (oldestvote < vote.time) {
+			oldestvote = vote.time;
+		}
+		goodvotes[k] = vote;
+		if (outcome[vote.vote]) {
+			outcome[vote.vote]++;
+		} else {
+			outcome[vote.vote] = 1;
+		}
+	}
+
+	this.votes = goodvotes;
+
+	console.log(outcome);
+	logfile.write(JSON.stringify({time:Date.now(), totals:outcome}) + '\n');
+	
+	outcome['centre-stop'] = 0;
+	
+	var winner = 'centre-stop';
+	var winnervotes = 0;
+	for (var k in outcome) {
+		if (!outcome.hasOwnProperty(k)) {
+			continue;
+		}
+		if (winnervotes < outcome[k]) {
+			winner = k;
+			winnervotes = outcome[k];
+		}
+	}
+
+	console.log('winner: ' + winner);
+
+	clearTimeout(this.runningtimer);		
+	if (0 < oldestvote) {
+		var that = this;
+		this.runningtimer = setTimeout(function() {that.recalculate();}, oldestvote + 10*1000 - Date.now());
+	}
+};
+
+
+var votes = new VoteCounter();
 
 var app = module.exports = express.createServer();
 
@@ -38,6 +104,7 @@ app.get('/setvote', function (req,res) {
 	var now = new Date();
 	console.log('vote ' + now.toISOString() + ' ' + user + ' ' + vote);
 	logfile.write(JSON.stringify({time:now.toISOString(), user:user, vote:vote}) + '\n');
+	votes.setVote(user, vote);
 	res.end('ok')
 
 }); 
